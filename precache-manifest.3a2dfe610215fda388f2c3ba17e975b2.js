ad61d4278f7d57a724a38082f6a7bde1b133c18b
AgnieszkaKurek <agniechakurek@gmail.com>
2019-03-21 16:26:18 +0100

    Updates

---
+++ precache-manifest.3a2dfe610215fda388f2c3ba17e975b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToeGameReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d659c85b9dd38a86ed04",
+    "url": "/TicTacToeGameReact/static/js/runtime~main.c16bca96.js"
+  },
+  {
+    "revision": "62767470a0fade2c0fa5",
+    "url": "/TicTacToeGameReact/static/js/main.fb47b125.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/TicTacToeGameReact/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "62767470a0fade2c0fa5",
+    "url": "/TicTacToeGameReact/static/css/main.b569b993.chunk.css"
+  },
+  {
+    "revision": "b70f2708445077c546a396eab3f88f9f",
+    "url": "/TicTacToeGameReact/index.html"
+  }
+];
