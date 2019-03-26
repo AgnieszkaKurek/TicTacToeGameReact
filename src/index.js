01312c2d0492c15d1a6b1c25346466c5783e8dde
AgnieszkaKurek <agniechakurek@gmail.com>
2019-03-26 11:03:28 +0100

    creat gameBord

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Box extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="box">
+      <canvas width="180" height="180" />
         {/* TODO */}
       </button>
     );
@@ -13,28 +14,28 @@ class Square extends React.Component {
   }
 
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    rendererBox(i) {
+      return <Box />;
     }
   
     render() {
       
       return (
-        <div>
+        <div className="gameBoard">
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.rendererBox(0)}
+            {this.rendererBox(1)}
+            {this.rendererBox(2)}
+            </div>
+            <div className="board-row">
+            {this.rendererBox(3)}
+            {this.rendererBox(4)}
+            {this.rendererBox(5)}
+            </div>
+            <div className="board-row">
+            {this.rendererBox(6)}
+            {this.rendererBox(7)}
+            {this.rendererBox(8)}
           </div>
         </div>
       );
@@ -44,13 +45,12 @@ class Square extends React.Component {
   class Game extends React.Component {
     render() {
       return (
-        <div className="game">
-          <div className="game-board">
+        <div className="container">
+        <header class="header">
+            <h1>Tic Tac Toe</h1>
+        </header>
+          <div >
             <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
           </div>
         </div>
       );
