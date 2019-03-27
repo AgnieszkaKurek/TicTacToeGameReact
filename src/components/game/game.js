b140f9990623db2761f26d3bc6e6d537d9e3cf16
AgnieszkaKurek <agniechakurek@gmail.com>
2019-03-27 13:23:47 +0100

    add reset button (#9)

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './game.css';
 import { Board } from '../board/board';
+import { Button } from '../button/button';
 
 export class Game extends React.Component {
   render() {
     return (
       <div className="container">
         <h1>Tic Tac Toe</h1>
+        <div className="game-area">
         <Board />
+        <Button/>
+        </div>
       </div>
     );
   }
