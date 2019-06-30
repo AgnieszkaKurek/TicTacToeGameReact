9cd18223bd0f6a3b2bbfeb8e77fb1779c2a446aa
AgnieszkaKurek <agniechakurek@gmail.com>
2019-06-30 17:40:29 +0200

    highlighting the corresponding component

--- src/components/punctation.js
+++ src/components/punctation.js
@@ -5,8 +5,12 @@ import { TicTacToeGameStatus } from '../models/ticTacToeGameStatus';
 export class Punctation extends React.Component {
 
 
-  getScoreClasses() {
-    const highlightClass = this.props.status !== TicTacToeGameStatus.STATUS_UNFINISHED
+  getScoreClasses(attribute) {
+    let status = this.props.status;
+    const highlightClass = status === TicTacToeGameStatus.STATUS_X_WINS &
+      attribute === TicTacToeGameStatus.STATUS_X_WINS ||
+      status === TicTacToeGameStatus.STATUS_O_WINS & attribute === TicTacToeGameStatus.STATUS_O_WINS ||
+      status === TicTacToeGameStatus.STATUS_DRAW & attribute === TicTacToeGameStatus.STATUS_DRAW
       ? "score-highlight"
       : "";
     return `space-between-elem ${highlightClass} `;
@@ -18,19 +22,19 @@ export class Punctation extends React.Component {
         <h2>Punctation</h2>
         <div>
           Score PlayerX
-          <span className={this.getScoreClasses()}>
+          <span className={this.getScoreClasses(TicTacToeGameStatus.STATUS_X_WINS)}>
             {this.props.scorePlayerX}
           </span>
         </div>
         <div>
           Score PlayerO
-          <span className={this.getScoreClasses()}>
+          <span className={this.getScoreClasses(TicTacToeGameStatus.STATUS_O_WINS)}>
             {this.props.scorePlayerO}
           </span>
         </div>
         <div>
           Number of draw
-          <span className={this.getScoreClasses()}>
+          <span className={this.getScoreClasses(TicTacToeGameStatus.STATUS_DRAW)}>
             {this.props.numberOfDraws}
           </span>
         </div>
