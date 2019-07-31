246eb1eeb113236a764242691b4e9ffdc716563a
AgnieszkaKurek <agniechakurek@gmail.com>
2019-07-31 19:40:42 +0200

    Feature/14 responsive design version2 (#41)
    * Unify h2 and add footer
    
    * Fix formatting
    
    * Change the way box is rendered
    
    * add responsive design for bame bord

--- src/components/playerInfo.js
+++ src/components/playerInfo.js
@@ -16,15 +16,28 @@ export class PlayerInfo extends React.Component {
     }
   }
 
+  rendererStatus() {
+    switch (this.props.status) {
+      case TicTacToeGameStatus.STATUS_X_WINS:
+        return <span>Winner is X</span>;
+      case TicTacToeGameStatus.STATUS_O_WINS:
+        return <span>Winner is O</span>;
+      case TicTacToeGameStatus.STATUS_DRAW:
+        return <span>Draw</span>;
+      default:
+        return;
+    }
+  }
+
   render() {
     if (this.props.status === TicTacToeGameStatus.STATUS_UNFINISHED) {
       return (
         <div>
-          <h2>Who's Turn</h2>
+          <h2 className="playerInfo">Who's Turn</h2>
           {this.renderPlayerInfo()}
         </div>
       );
     }
-    return (<div></div>);
+    return (<div className="status">{this.rendererStatus()}</div>);
   }
 }
